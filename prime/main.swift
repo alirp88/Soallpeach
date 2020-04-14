7d7498aa0c585c959440105e4fecd80a14712668
alirp88 <riahipour.ali@gmail.com>
2020-04-14 09:25:41 +0200

    check till sqrt self

--- prime/main.swift
+++ prime/main.swift
@@ -2,7 +2,16 @@ import Foundation
 
 extension Int {
     var isPrime: Bool {
-        for i in 2..<self {
+        if self < 3 {
+            return true
+        }
+        if self == 4 {
+            return false
+        }
+        if self == 5 {
+            return true
+        }
+        for i in 2..<(Int(sqrt(Double(self))) + 1) {
             if self % i == 0 {
                 return false
             }
